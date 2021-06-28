8990164ecd067e80a0c81a79ba623e0268432d36
Manoj A M <manojavarur@gmail.com>
2021-06-29 00:31:14 +0530

    Trying to connect to database using HTTP and client

--- HttpRequestUsingGET/sketch_jun29a/sketch_jun29a.ino
+++ HttpRequestUsingGET/sketch_jun29a/sketch_jun29a.ino
@@ -20,16 +20,15 @@ void loop() {
 void insert_into_database(){
   HTTPClient http;
 
-  http.begin(client, "http://iotproject.ezyro.com/iotest/connection.php");
+  http.begin(client, "http://iotproject.ezyro.com/iotest/connection.php?val1=2323.434&val2=356.8889");
   
   http.addHeader("Content-Type", "application/x-www-form-urlencoded");
   
-  int httpCode = http.POST("?sendval=" + String( random(1, 401) / 100.0 ) + 
-                           "&sendval2="+ String( random(1, 401) / 100.0 ) );
+  int httpCode = http.POST("");
                            
   String payload = http.getString();                  
   Serial.println(httpCode);   
-//  Serial.println(payload); 
+  Serial.println(payload); 
   
   http.end();
   Serial.print("Completed till here!!!");
