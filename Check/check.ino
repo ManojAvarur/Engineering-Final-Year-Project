52560275a6d1f4c276df6042836b713951437a1b
Manoj A M <manojavarur@gmail.com>
2021-07-13 22:58:35 +0530

    Nothing here

---
+++ Check/check.ino
@@ -0,0 +1,69 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+#include <ArduinoJson.h>
+StaticJsonDocument<48> Json_result_responce;
+
+const char* ssid = "Manoj";
+const char* password = "Hazelnut+-";
+WiFiClient client;
+
+void setup_wifi();
+int nodemcu_freeze_check();
+
+
+
+
+
+
+
+void setup(){
+  Serial.begin(9600);
+  setup_wifi();
+}
+
+void loop(){
+  nodemcu_freeze_check();
+  delay(3000);
+}
+
+int nodemcu_freeze_check(){
+  int httpCode;
+String result;
+  do{
+    HTTPClient http;
+    http.begin(client, "http://iotproject.coolpage.biz/handel_requests/iotrequest/db_nodemcu_freeze_check.php?");
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    httpCode = http.POST("uid=ccac34d923330a2968f12e163d5a2cd6");
+    result = http.getString(); 
+
+    http.end();
+
+    Serial.println("Responce : "+ result + "\nResponce Code : " + String( httpCode ) );
+    
+    if( httpCode != 200 ){
+        delay(1000);
+    }
+    
+  }while( httpCode != 200 );
+
+//  deserializeJson(Json_result_responce, result);
+//  result = Json_result_responce["result"].as<String>();
+//  Serial.println( Json_result_responce["result"].as<String>() );
+//  Json_result_responce.clear();
+//  Json_result_responce.delete();
+
+}
+
+void setup_wifi(){
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+}
